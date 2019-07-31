b1183f38a76849a21535cb79674b2ef474cf605b
kitetsu7 <javiertinocodiaz@yahoo.com.mx>
2019-07-30 22:50:13 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Hola Mundo!! developer jr is leaving Testing Github Webhook for automatic build</h1>
+                <h1>Hola Mundo!! developer jr is now leaving Testing Github Webhook for automatic build</h1>
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
